9ea01e44a474e4fa5323e69f90e763b63de88a63
Arvin Lim <arvinlim3675@gmail.com>
2023-01-09 10:17:53 +1100

    Add group by col option to lag

--- src/helpers.py
+++ src/helpers.py
@@ -3,12 +3,16 @@ def clean_col_names(df):
     df.columns=df.columns.str.lower().str.replace(' ','_')
     return df
 
-def add_lag(df, column_name, lags):
+def add_lag(df, column_name, lags, group_by_col='melid'):
     for lag in lags:
         if lag < 0:
             name = "lead"
         else:
             name = "lag"
 
-        df[f'{column_name}_{name}_{abs(lag)}'] = df.groupby('melid')[column_name].shift(lag)
+        if group_by_col is not None:
+            df[f'{column_name}_{name}_{abs(lag)}'] = df.groupby(group_by_col)[column_name].shift(lag)
+        else:
+            df[f'{column_name}_{name}_{abs(lag)}'] = df[column_name].shift(lag)
+
     return df
